2c3b758fdf74ddcd9b106a2f54b70601f26e443f
Francesco Dorati <francesco.dorati@mail.polimi.it>
2025-07-18 23:18:55 +0200

    saved schematics & formatted

--- src/main.ino
+++ src/main.ino
@@ -2,84 +2,69 @@
 #include <SoftwareSerial.h>
 
 const int COMM_PIN = 2;
-const int RX_PIN = 10; // RX pin for RS485
-const int TX_PIN = 11; // TX pin for RS485
+const int RX_PIN = 10;  // RX pin for RS485
+const int TX_PIN = 11;  // TX pin for RS485
 
-class RS485Server
-{
-public:
-    RS485Server(int comm, int rx, int tx) : comm(comm), serial(rx, tx) {}
+class RS485Server {
+ public:
+  RS485Server(int comm, int rx, int tx) : comm(comm), serial(rx, tx) {}
 
-    void begin(long baudrate)
-    {
-        serial.begin(baudrate);
-        pinMode(comm, OUTPUT);
-        digitalWrite(comm, LOW);
-    }
+  void begin(long baudrate) {
+    serial.begin(baudrate);
+    pinMode(comm, OUTPUT);
+    digitalWrite(comm, LOW);
+  }
 
-    bool available()
-    {
-        return serial.available();
-    }
+  bool available() { return serial.available(); }
 
-    String receiveResponse()
-    {
-        String command = serial.readStringUntil('\n');
-        command.trim();
-        return command;
-    }
+  String receiveResponse() {
+    String command = serial.readStringUntil('\n');
+    command.trim();
+    return command;
+  }
 
-    void transmitTo(String target, String message)
-    {
-        digitalWrite(comm, HIGH);
-        delayMicroseconds(10);
-        serial.println((target + " " + message).c_str());
-        serial.flush();
-        digitalWrite(comm, LOW);
-        Serial.println("sent: " + message);
-    }
+  void transmitTo(String target, String message) {
+    digitalWrite(comm, HIGH);
+    delayMicroseconds(10);
+    serial.println((target + " " + message).c_str());
+    serial.flush();
+    digitalWrite(comm, LOW);
+    Serial.println("sent: " + message);
+  }
 
-private:
-    int comm;
-    SoftwareSerial serial;
+ private:
+  int comm;
+  SoftwareSerial serial;
 };
 
 RS485Server rs485(COMM_PIN, RX_PIN, TX_PIN);
 
-void setup()
-{
-    Serial.begin(9600);
-    rs485.begin(9600);
-    Serial.println("START\n");
+void setup() {
+  Serial.begin(9600);
+  rs485.begin(9600);
+  Serial.println("START\n");
 }
 
-void loop()
-{
-    if (Serial.available())
-    {
-        String command = Serial.readStringUntil('\n');
-        rs485.transmitTo("*", command);
-    }
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    rs485.transmitTo("*", command);
+  }
 
-    if (rs485.available())
-    {
-        String response = rs485.receiveResponse();
-        Serial.println("received: " + getBody(response) + "\n");
-    }
+  if (rs485.available()) {
+    String response = rs485.receiveResponse();
+    Serial.println("received: " + getBody(response) + "\n");
+  }
 }
 
-String getRecipient(String command)
-{
-    int spaceIndex = command.indexOf(' ');
-    if (spaceIndex == -1)
-        return "";
-    return command.substring(0, spaceIndex);
+String getRecipient(String command) {
+  int spaceIndex = command.indexOf(' ');
+  if (spaceIndex == -1) return "";
+  return command.substring(0, spaceIndex);
 }
 
-String getBody(String command)
-{
-    int spaceIndex = command.indexOf(' ');
-    if (spaceIndex == -1)
-        return "";
-    return command.substring(spaceIndex + 1);
+String getBody(String command) {
+  int spaceIndex = command.indexOf(' ');
+  if (spaceIndex == -1) return "";
+  return command.substring(spaceIndex + 1);
 }
--- src/plant.ino
+++ src/plant.ino
@@ -1,149 +1,116 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-const String PLANT_ID = "0"; // Unique ID for this plant
+const String PLANT_ID = "0";  // Unique ID for this plant
 
 const int SENSOR_PIN = A0;
 const int LED_PIN = 2;
-const int COMM_PIN = 3; // LOW: listening, HIGH: sending
-const int PUMP_PIN = 4; // LOW: ON, HIGH: OFF
-const int RX_PIN = 10;  // RX pin for RS485
-const int TX_PIN = 11;  // TX pin for RS485
+const int COMM_PIN = 3;  // LOW: listening, HIGH: sending
+const int PUMP_PIN = 4;  // LOW: ON, HIGH: OFF
+const int RX_PIN = 10;   // RX pin for RS485
+const int TX_PIN = 11;   // TX pin for RS485
 
-class LED
-{
-public:
+class LED {
+ public:
   LED(int p) : pin(p) {}
 
-  void begin()
-  {
+  void begin() {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
 
-  void setOn()
-  {
-    digitalWrite(pin, HIGH);
-  }
+  void setOn() { digitalWrite(pin, HIGH); }
 
-  void setOff()
-  {
-    digitalWrite(pin, LOW);
-  }
+  void setOff() { digitalWrite(pin, LOW); }
 
-  void blink(int seconds)
-  {
+  void blink(int seconds) {
     setOn();
     delay(seconds * 1000);
     setOff();
   }
 
-private:
+ private:
   int pin;
 };
 
-class HumiditySensor
-{
-public:
+class HumiditySensor {
+ public:
   HumiditySensor(int p) : pin(p) {}
 
   void begin() { pinMode(pin, INPUT); }
 
-  int getHumidity()
-  {
+  int getHumidity() {
     return 100.0 * (getVoltage() - DRY_VOLTAGE) / (WET_VOLTAGE - DRY_VOLTAGE);
   }
 
-  float getVoltage()
-  {
-    return (getValue() * MAX_VOLTAGE) / 1023.0;
-  }
+  float getVoltage() { return (getValue() * MAX_VOLTAGE) / 1023.0; }
 
-  int getValue()
-  {
-    return analogRead(pin);
-  }
+  int getValue() { return analogRead(pin); }
 
-private:
+ private:
   int pin;
   const float MAX_VOLTAGE = 5.0;
-  const float DRY_VOLTAGE = 3.27; // 3.04 on local
-  const float WET_VOLTAGE = 1.18; // 1.11 on local
+  const float DRY_VOLTAGE = 3.27;  // 3.04 on local
+  const float WET_VOLTAGE = 1.18;  // 1.11 on local
 };
 
-class Pump
-{
-public:
+class Pump {
+ public:
   Pump(int p) : pin(p), on(false) {}
 
-  void begin()
-  {
+  void begin() {
     pinMode(pin, OUTPUT);
-    digitalWrite(pin, HIGH); // Start with pump OFF
+    digitalWrite(pin, HIGH);  // Start with pump OFF
   }
 
-  void setOn()
-  {
-    digitalWrite(pin, LOW); // Turn pump ON
+  void setOn() {
+    digitalWrite(pin, LOW);  // Turn pump ON
     on = true;
   }
 
-  void setOff()
-  {
-    digitalWrite(pin, HIGH); // Turn pump OFF
+  void setOff() {
+    digitalWrite(pin, HIGH);  // Turn pump OFF
     on = false;
   }
 
-  void isOn()
-  {
-    return on;
-  }
+  void isOn() { return on; }
 
-  void activate(int seconds)
-  {
+  void activate(int seconds) {
     setOn();
     delay(seconds * 1000);
     setOff();
   }
 
-private:
+ private:
   int pin;
   bool on;
 };
 
-class RS485Client
-{
-public:
-  RS485Client(String id, int comm, int rx, int tx) : id(id), comm(comm), serial(rx, tx) {}
+class RS485Client {
+ public:
+  RS485Client(String id, int comm, int rx, int tx)
+      : id(id), comm(comm), serial(rx, tx) {}
 
-  void begin(long baudrate)
-  {
+  void begin(long baudrate) {
     serial.begin(baudrate);
     pinMode(comm, OUTPUT);
     digitalWrite(comm, LOW);
   }
 
-  bool available()
-  {
-    return serial.available();
-  }
+  bool available() { return serial.available(); }
 
-  String receiveCommand()
-  { // Ensure command is from this plant
+  String receiveCommand() {  // Ensure command is from this plant
     String command = serial.readStringUntil('\n');
 
     command.trim();
-    if (command.equals(""))
-      return "";
+    if (command.equals("")) return "";
 
     String recipient = getRecipient(command);
-    return (recipient.equals(id) || recipient.equals("*"))
-               ? getBody(command)
-               : "";
+    return (recipient.equals(id) || recipient.equals("*")) ? getBody(command)
+                                                           : "";
   }
 
-  void transmit(String message)
-  {
+  void transmit(String message) {
     digitalWrite(comm, HIGH);
     delayMicroseconds(10);
     serial.println((id + " " + message).c_str());
@@ -152,37 +119,30 @@ public:
     Serial.println("sent: " + message);
   }
 
-private:
+ private:
   String id;
   int comm;
   SoftwareSerial serial;
 
-  String getRecipient(String command)
-  {
+  String getRecipient(String command) {
     int spaceIndex = command.indexOf(' ');
-    if (spaceIndex == -1)
-      return "";
+    if (spaceIndex == -1) return "";
     return command.substring(0, spaceIndex);
   }
 
-  String getBody(String command)
-  {
+  String getBody(String command) {
     int spaceIndex = command.indexOf(' ');
-    if (spaceIndex == -1)
-      return "";
+    if (spaceIndex == -1) return "";
     return command.substring(spaceIndex + 1);
   }
 };
-
-// SoftwareSerial RS485(RX_PIN, TX_PIN);
 
 RS485Client rs485(PLANT_ID, COMM_PIN, RX_PIN, TX_PIN);
 LED led(LED_PIN);
 HumiditySensor humiditySensor(SENSOR_PIN);
 Pump pump(PUMP_PIN);
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   rs485.begin(9600);
@@ -194,75 +154,56 @@ void setup()
   Serial.println("START");
 }
 
-void loop()
-{
+void loop() {
   // RS485 communication
-  if (rs485.available())
-  {
-    String command = rs485.receiveCommand(); // Ensure command is for this plant
-    if (!command.equals(""))
-    {
+  if (rs485.available()) {
+    String command =
+        rs485.receiveCommand();  // Ensure command is for this plant
+    if (!command.equals("")) {
       interpretCommand(command);
     }
   }
 
   // Serial communication (DEBUG)
-  if (Serial.available())
-  {
+  if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     Serial.println("debugging: \"" + command + "\"");
     interpretDebug(command);
   }
 }
 
-void interpretCommand(String command)
-{
-  if (command.equals("PING"))
-  {
+void interpretCommand(String command) {
+  if (command.equals("PING")) {
     rs485.transmit("PONG");
     led.blink(1);
-  }
-  else if (command.equals("HUMIDITY"))
-  {
+  } else if (command.equals("HUMIDITY")) {
     int humidity = humiditySensor.getHumidity();
-    rs485.transmit(String(humidity) + " " + String(humiditySensor.getVoltage()));
-  }
-  else if (command.startsWith("WATER"))
-  {
+    rs485.transmit(String(humidity) + " " +
+                   String(humiditySensor.getVoltage()));
+  } else if (command.startsWith("WATER")) {
     int seconds = command.substring(6).toInt();
-    if (seconds < 0)
-      return; // Invalid command
+    if (seconds < 0) return;  // Invalid command
     pump.activate(seconds);
-  }
-  else
-  {
+  } else {
     Serial.println("Unknown command: \"" + command + "\"");
   }
 }
 
-void interpretDebug(String command)
-{
-  if (command.equals("PING"))
-  {
+void interpretDebug(String command) {
+  if (command.equals("PING")) {
     Serial.println("PONG");
     led.blink(1);
-  }
-  else if (command.equals("HUMIDITY"))
-  {
+  } else if (command.equals("HUMIDITY")) {
     int value = humiditySensor.getValue();
     float voltage = humiditySensor.getVoltage();
     int humidity = humiditySensor.getHumidity();
-    Serial.println("Humidity: " + String(humidity) + "%, Voltage: " + String(voltage) + "V, value: " + String(value));
-  }
-  else if (command.startsWith("WATER"))
-  {
+    Serial.println("Humidity: " + String(humidity) + "%, Voltage: " +
+                   String(voltage) + "V, value: " + String(value));
+  } else if (command.startsWith("WATER")) {
     int seconds = command.substring(6).toInt();
-    if (seconds < 0)
-      return; // Invalid command
+    if (seconds < 0) return;  // Invalid command
     pump.activate(seconds);
-  }
-  else
-  {
+  } else {
     Serial.println("Unknown command: " + command);
   }
 }
